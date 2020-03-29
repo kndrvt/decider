b8fbc206eeaab99c040de08a4099c9666b3442ed
kndrvt <kndrvt@yandex.ru>
2020-03-29 21:28:26 +0500

    HTTP Parsing

--- DeciderClient.py
+++ DeciderClient.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+# Quagga
 import ciscotelnet
-from scapy.sendrecv import sniff
-from scapy.layers.http import HTTP, HTTPRequest, HTTPResponse
-from scapy.layers.inet import TCP, TCP_client
-from scapy.sessions import TCPSession
+
+# Scapy
+from scapy.all import *
+from scapy.layers.http import *
 
 
 class TelnetQuagga:
@@ -20,12 +21,30 @@ class TelnetQuagga:
 
 
 if __name__ == '__main__':
-    tq = TelnetQuagga(host="127.0.0.1", port=2605)
-    print(tq.router.cmd("sh ip bgp"))
-    print("")
+    # tq = TelnetQuagga(host="127.0.0.1", port=2605)
+    # print(tq.router.cmd("sh ip bgp"))
+    # print("")
 
     pkts = sniff(offline="invoke.pcap", filter='tcp port 8080', session=TCPSession)
     for pkt in pkts:
-        if pkt[TCP].payload:
-            pkt.show()
-            print("==================================================")
+        http = pkt[TCP].payload
+        if http:
+            print("=================================")
+            httpr = http.payload
+            print('Type:', httpr.name)
+
+            if httpr.name == 'HTTP Request':
+                print('Method:', httpr.Method.decode('ascii'))
+                print('Host:', httpr.Host.decode('ascii'))
+                print('Path:', httpr.Path.decode('ascii'))
+                print('Payload:', httpr.payload.load.decode('ascii'))
+
+            elif httpr.name == 'HTTP Response':
+                print('Status code:', httpr.Status_Code.decode('ascii'))
+                print('Payload:', httpr.payload.load.decode('ascii'))
+
+            else:
+                pass
+
+            print("=================================")
+            print()
