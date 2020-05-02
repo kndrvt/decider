053d2a7fd448a3b0857155a7d42a9cf5da0410fb
kndrvt <kndrvt@yandex.ru>
2020-05-02 22:49:49 +0500

    Add client.py

---
+++ client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import signal
+import numpy as np
+import time
+
+
+def signalHandler(signum, frame):
+    print()
+    print('=== Openfaas server stopping ===')
+    exit()
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print('Usage: sudo python3 client.py <IP address>')
+        exit()
+
+    input = "default"
+    if len(sys.argv) > 2:
+        input = " ".join(sys.argv[2:])
+
+    print()
+    print('=== Openfaas server starting ===')
+    signal.signal(signal.SIGINT, signalHandler)
+    np.random.seed()
+
+    while True:
+        os.system("echo \"" + input + "\" | faas-cli -g " + sys.argv[1] + ":8080 invoke figlet")
+        time.sleep(0.01 * np.random.poisson(lam=100))
